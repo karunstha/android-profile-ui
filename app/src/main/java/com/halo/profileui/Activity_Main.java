bacd392f9e6c10c5466f2752008bd17d1eb93edc
Karun Shrestha <kars.kit@gmail.com>
2020-05-19 23:20:50 +0545

    first commit

---
+++ app/src/main/java/com/halo/profileui/Activity_Main.java
@@ -0,0 +1,13 @@
+package com.halo.profileui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
